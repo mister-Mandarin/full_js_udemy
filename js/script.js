416d3ef06bd6815dd5771eea2cdab856c5902ed5
Andrey <test@yandex.ru>
2021-12-13 10:44:26 +0300

    Сделала первое задание

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidfth = 1;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const obj = {
-    name: "Jon",
-    age: 10,
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [],
+    privat: false
 };
 
-console.log(obj.age);
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
